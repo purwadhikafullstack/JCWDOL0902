78509eb243bfb28f6c7647d91f4745a3f70c0fa6
Rahmat Totong <rahmattotong18@gmail.com>
2023-06-07 17:52:58 +0800

    Admin Dashboard Commit

---
+++ projects/client/src/components/Admin/AdminDrawerResponsive.jsx
@@ -0,0 +1,41 @@
+import {
+    Drawer,
+    DrawerContent,
+    DrawerCloseButton,
+    useDisclosure,
+    Box,
+} from "@chakra-ui/react";
+
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { DrawerItems } from "./AdminDrawerItems";
+
+export const DrawerAdminResponsive = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    return (
+        <Box>
+            <HamburgerIcon
+                color={"white"}
+                display={{ base: "flex" }}
+                onClick={onOpen}
+                variant="outline"
+                aria-label="open menu"
+                w={6}
+                h={6}
+            />
+            <Drawer
+                autoFocus={false}
+                isOpen={isOpen}
+                placement="left"
+                onClose={onClose}
+                returnFocusOnClose={false}
+                onOverlayClick={onClose}
+                size={"xs"}
+            >
+                <DrawerContent bg={"#343A40"}>
+                    <DrawerCloseButton color={"white"} />
+                    <DrawerItems onClose={onClose} />
+                </DrawerContent>
+            </Drawer>
+        </Box>
+    );
+};
