f0d1623d8f38ecec2db1661975a85948079b7be6
Rahmat Totong <rahmattotong18@gmail.com>
2023-06-19 13:52:04 +0800

    Update Banner.jsx

--- projects/client/src/components/Banner.jsx
+++ projects/client/src/components/Banner.jsx
@@ -1,18 +1,3 @@
-// import { Flex, Image, Box, Center } from "@chakra-ui/react";
-// import banner from "../assets/banner_hobby_zone.jpg";
-
-// export const Banner = () => {
-//   return (
-//     <Center>
-//       <Box maxW="100%" borderTop="5px solid black" borderBottom="5px solid black">
-//         <Flex justify="center" alignItems="center">
-//           <Image src={banner} alt="Banner" borderRadius="lg" objectFit="cover" width="100%" />
-//         </Flex>
-//       </Box>
-//     </Center>
-//   );
-// };
-
 import { Flex, Image, Box, Center } from "@chakra-ui/react";
 import banner from "../assets/banner_hobby_zone.jpg";
 
